64c4085d253c5d89f3f8477f0e80328347cf05ee
Rishi Sharma <63443330+Rishi-Sharma2002@users.noreply.github.com>
2021-01-01 13:53:52 +0530

    Create AddTwoNumbers.cpp

---
+++ CodeChef/Beginner/AddTwoNumbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+  int n;
+  cin>>n;
+  
+  while(n--){
+    int a, b, arr[10];
+    arr[]
+    
+    }
+
+  return 0;
+  }
