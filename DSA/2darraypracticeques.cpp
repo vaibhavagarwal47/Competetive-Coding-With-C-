f82ca43cc7b2a1fa0c8f9c4bb20c75279caa84e5
vaibhavagarwal47 <58652650+vaibhavagarwal47@users.noreply.github.com>
2021-01-29 14:54:34 +0530

    change

--- DSA/2darraypracticeques.cpp
+++ DSA/2darraypracticeques.cpp
@@ -166,3 +166,68 @@ int main() {
 */
 
 /*
+//print
+    *
+   **
+  ***
+ ****
+*****
+#include <iostream>
+using namespace std;
+int main() {
+   int n;
+   cin>>n;
+   int a[n][n];
+   for(int i=0;i<n;i++){
+       for(int j=0;j<n;j++){
+           cin>>a[i][j];
+       }
+   }
+    for(int i=0;i<n;i++){
+        for(int k=i+1;k<n;k++){
+            cout<<" ";
+        }
+        for(int j=n-i-1;j<n;j++){
+            cout<<a[i][j];
+        }
+        cout<<endl;
+    }
+   return 0;
+}
+*/
+
+/*
+//print 
+*****
+****
+***
+**
+*
+#include <iostream>
+using namespace std;
+int main() {
+   int n;
+   cin>>n;
+   int a[n][n];
+   for(int i=0;i<n;i++){
+       for(int j=0;j<n;j++){
+           cin>>a[i][j];
+       }
+   }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<a[i][j];
+        }
+        cout<<endl;
+    }
+   return 0;
+}
+*/
+
+/*
+//print
+*****
+*   *
+*   *
+*   *
+*****
